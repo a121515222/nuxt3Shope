f0d75a90a4306e9723abbf9f3bed83dff7f8e3b8
CHUN-CHIA <a121515222@gmail.com>
2025-03-20 03:31:18 +0800

    fix:修復首頁照片消失

--- pages/index.vue
+++ pages/index.vue
@@ -39,15 +39,16 @@ if (process.server) {
 const bannerConfig = {
   duration: 6000,
   imagesPath: [
-    "https://storage.googleapis.com/vue-course-api.appspot.com/chun-chia/1652259953185.jpg?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=SiEnRDMhsYt%2Bv98iDemtQRTjL5WG1Y0AuxwgZTiJM9FU9q5WTzkpjB0xkghzcPVGEEaLChFR6uAe2TI59nQJhoBoKcK3kW64YleYC9ITfDZdMHe9M833Kzd7t0lAS5UCR0h8tUNbQnmoWh9JbfnjM3Pf%2BYvc14e912vBgKGL6oesX6rjTfk79S%2FiX2aVszTSf3D3mEWZZ%2BXtyPwghbBFjl7n2BoEtzXFIk%2B%2B49mcbgbHWw%2FMpoeTNfSFKYK4qVoVSoXrC15wVoB3FReLzSLHupyGPMzcPi0qKb51HyBoLmFznvTxymoOUHBP%2FawPbQ9o7%2BkdHsDioaGbJwIGt29gnA%3D%3D",
-    "https://storage.googleapis.com/vue-course-api.appspot.com/chun-chia/1652260184963.jpg?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=juhYb%2FKhH1KWiij7g9tU4voNPdpyDTZtNIv49R0W8554DsSBmkqci3Bd0iOp250NMR1sMgGB9coee80SJ0CeoP%2FaKesb5SotKZRVvl1W56ym6G3W1mCYMU4WORjT5Zqeny0%2Bao0hOZXCXQg4S5T6oJh6XksRjeOg8ilCL0FePTOhhjoejsTmRCKycmaUSoS7VismMPvDu8B6c7apgFESWGM59dKlu60M%2Bome3hvsJsUwpvXnShhKQ6h4oj56jpirqMxczxE5joTagHYbNZYeMxcG%2Bzl64vMu%2Fz%2F4%2FnI0XgLmXOG%2FdjWNGZ0fzsoRK3UPMA6lydQAJifWw3afW4FoSg%3D%3D",
-    "https://storage.googleapis.com/vue-course-api.appspot.com/chun-chia/1652260045306.jpg?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=YG9ZpKhC%2F9WFYAlqCTDJ1sijGMTONgAeEG2DFkI6Z%2FssrMX968TP075wxK8G3%2FBmRp9IkMJrVNDqkRVjY5zjar1wGhlCHUtDqKX%2FAnYAEJEyZjg9N8BcyoztpPgw5RP90kzY8eqrtrA3uKHR0kVX3yNoauNbXQX1OJ67Mtc9hG2fj%2FiXhP%2BIxkiCxbQ9f5OeCJaxLzkwnyNJxmcNvTfM23XfN3YL8nTih61uvwrBZtAK%2Bknuh%2F1%2BlyE5VrXKmB%2BiIAAxe6dcoqcPzwOppiPonf7VYB8PNNeLRxITXkHHtz4Q3IczUeusPo1LPXx3Rz0blmpSSqQOJEX17FeH7D7aiw%3D%3D"
+    "https://firebasestorage.googleapis.com/v0/b/imagestorge-b6395.appspot.com/o/vueShop%2FIMG_9359.JPG?alt=media&token=48410a79-62b5-444a-a435-87a0778051fc",
+    "https://firebasestorage.googleapis.com/v0/b/imagestorge-b6395.appspot.com/o/vueShop%2FDuWGTAnc8x7CWX9t-generated_image.jpg?alt=media&token=b14a24a6-c30a-48e8-852b-497e872e0b22",
+    "https://firebasestorage.googleapis.com/v0/b/imagestorge-b6395.appspot.com/o/vueShop%2FRlh3PIsGcf5fOeRc-generated_image.jpg?alt=media&token=5761ace9-2461-49f4-a80b-e62650eaad45",
+    "https://firebasestorage.googleapis.com/v0/b/imagestorge-b6395.appspot.com/o/vueShop%2FZBdgw33tP85J6wX9-generated_image.jpg?alt=media&token=5238df68-d25f-4f3c-a7be-f5ffedcacf33"
   ],
   isMainBanner: true
 };
 const subBannerConfig = {
   imagesPath: [
-    "https://storage.googleapis.com/vue-course-api.appspot.com/chun-chia/1652260099287.jpg?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=ZRPB2H%2BL4eMyzETsJo6S9hxSXmiZMwWFS7r9OW5dJ%2BaAFtE59RzAr50pO1rVS3PvUeKaPH4U6sUHlXOzHAJxpBYOgg3rHfmMuyjlg4qUwD%2FS1GhVzxGxfu%2BRmhFRnJFSMjgyvI7Zcx0g3zpvxvgSDWGxO3uSuSYUqOHUKdnvRzvE%2FbYF0iJN82lxr9UG9vcl9O0TNIlLf%2F%2FCFxAqsKTDvNUmumsoH3cVaI8XutbqO%2FMqqN0txbvbKZqT0t%2Bm1c%2FOM78S5O5Ggbksc3k8POKqWBvV7N%2F8MwhD57KgKBGjTw8qtrAxmgvduEaFO%2B8M3CqIEIK0Jw83Md1LHUIwi9I5wA%3D%3D"
+    "https://firebasestorage.googleapis.com/v0/b/imagestorge-b6395.appspot.com/o/vueShop%2Fzj64QUx9EY8aDnx0-generated_image.jpg?alt=media&token=ac716598-cecf-40ba-9bfe-9fa5e03070b0"
   ],
   isMainBanner: false,
   bannerHeight: "30vh",
@@ -55,7 +56,7 @@ const subBannerConfig = {
 };
 const buttonBannerConfig = {
   imagesPath: [
-    "https://storage.googleapis.com/vue-course-api.appspot.com/chun-chia/1652260363517.jpg?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=E%2FkEzKyp%2BtIyg6DGjzDAXAlAwrImtalmqjoH8DvwudNk49puvmRmAdtQTS1SL%2FvHhARiw%2FuhiHG0ICkOlHpft6NRk0hgKaHEVi1TyApl62rGJPXDJpOHwZRkiVPzoIqO8R2SY7YIusJBsyZmf8cI7Hr4S5BXBGupValaxIz2p%2FtZv4jj3a2DUZMuyPK8yHZF00V7UxooszeX6OiBQhFMxnIYjvgXHkQuV6OHJq3%2BUjdXW0ubLIq7TI%2FuXzd3bOieFLiTwu8QyI8%2BLeQjXIdywEqkUycavR2zWaNZwlxHiJArhQHvmfuO%2Fot5P%2BnQJeX5uz09m%2FuL1JDZz2KFk56IBQ%3D%3D"
+    "https://firebasestorage.googleapis.com/v0/b/imagestorge-b6395.appspot.com/o/vueShop%2FDuWGTAnc8x7CWX9t-generated_image.jpg?alt=media&token=b14a24a6-c30a-48e8-852b-497e872e0b22"
   ],
   isMainBanner: false,
   bannerHeight: "30vh",
